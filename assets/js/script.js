5ca7cee36b249b73d5288365f7f811bae276b428
Destiny <dejenkins@justworks.com>
2024-05-02 18:48:02 -0400

    additions to the droppable/draggble functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,17 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-  //think of drivers licenses and DMV visits, what if you have the same DOB of others? How does the state identify you. They need a reference point that has all your items
-  //what if two taks have the same title, due date, and description; how else are we identifying the task?  
-  // check if nextId exists(if it has a number) 
-    // if it does we can add 1
-    // if it doesn't we can make it 1
-    // lastly, put it in localStorage and return it
-    if (nextId === null) {
-      nextId = 1;
-    }
-    else {
-      nextId++;
-    }
-    JSON.stringify(localStorage.setItem("nextId", nextId))
-    return nextId;
+  if (nextId === null) {
+    nextId = 1;
+  } else {
+    nextId++;
+  }
+  JSON.stringify(localStorage.setItem("nextId", nextId));
+  return nextId;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
   const article = document.createElement("article");
   const taskTitleEl = document.createElement("h2");
   const taskDueDateEl = document.createElement("p");
@@ -39,7 +31,7 @@ function createTaskCard(task) {
   article.appendChild(taskDueDateEl);
   article.appendChild(taskDescriptionEl);
 
-return article;
+  return article;
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -48,8 +40,8 @@ function renderTaskList() {
     const task = taskList[index];
     const taskCard = createTaskCard(task);
     // if the status is toDo put it in the toDo column
-    if (task.status === "toDo"){
-      document.getElementById("todo-cards").appendChild(taskCard)
+    if (task.status === "toDo") {
+      document.getElementById("todo-cards").appendChild(taskCard);
     }
     // if the status is inProgress put it in the inProgress column
     if (task.status === "inProgress"){
@@ -60,7 +52,6 @@ function renderTaskList() {
       document.getElementById("done-cards").appendChild(taskCard)
     }
   }
-
 }
 
 // Todo: create a function to handle adding a new task
@@ -80,21 +71,30 @@ function handleAddTask(event) {
   // Add it to the task list in local storage
   taskList.push(newTask);
   localStorage.setItem("tasks", JSON.stringify(taskList));
-//Render the taskList
-renderTaskList();
-
+  //Render the taskList
+  renderTaskList();
 }
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event) {}
 
-
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-  console.log(event)
-  console.log(ui)
-  //update the status of the dropped task
-}
+  $( "#draggable" ).draggable();
+  
+  $( "#droppable" ).droppable({
+    drop: function( event, ui ) {
+      $( this )
+        .addClass( "todo-cards" )
+        .find( "article" )
+          .html( "Dropped!" );
+    }
+  });
+};
+  // console.log(event);
+  // console.log(ui);
+  // // update the status of the dropped task
+
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
@@ -103,10 +103,10 @@ $(document).ready(function () {
   document
     .querySelector("#save-changes")
     .addEventListener("click", handleAddTask);
-    $( ".lane" ).droppable({
-      accept: '.draggable',
-      drop: handleDrop,
-    });
-    // use mini project and documentation to configure dragging
-    $(".task-card").draggable();
+  $(".lane").droppable({
+    accept: '.draggable',
+    drop: handleDrop,
+  });
+  // use mini project and documentation to configure dragging
+  $(".task-card").draggable();
 });
